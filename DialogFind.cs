099fcf4173ff0ba7b078d9a4251dad1a0cb5e82e
mch2112 <mchamilton2112@gmail.com>
2016-09-07 13:27:57 -0700

    Add files via upload
    Upload #1

---
+++ DialogFind.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+internal class DialogFind : Dialog
+{
+    public DialogFind()
+	{
+	}
+}
---
+++ SolarMaxStart/SolarMaxStart/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SolarMaxStart
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            if (!isFrameworkOK())
+            {
+                if (MessageBox.Show("This program requires Microsoft .Net Framework 4 (Client Profile). Click OK to go download this file from microsoft.com",
+                                    ".Net Framework 4",
+                                    MessageBoxButtons.OKCancel,
+                                    MessageBoxIcon.Exclamation) == DialogResult.OK)
+                {
+                    System.Diagnostics.Process.Start(@"http://www.microsoft.com/downloads/en/details.aspx?displaylang=en&FamilyID=5765d7a8-7722-4888-a970-ac39b33fd8ab");
+                }
+                return;
+            }
+            else
+            {
+                System.Diagnostics.Process.Start(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Application.ExecutablePath), "SolarMax.exe"));
+            }
+        }
+
+        private static bool isFrameworkOK()
+        {
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client");
+            return regKey != null && regKey.GetValue("Install", "0").ToString() == "1";
+        }
+    }
+}
